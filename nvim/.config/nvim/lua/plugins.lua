b799737b933c9f5b8c791c58ec4955997a65606f
Tony Doan <tdoan@tdoan.com>
2025-04-14 20:39:19 -0700

    Add vim-repeat and vim-rustfmt

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -3,6 +3,8 @@ return {
     {"tpope/vim-surround"},
     {"tpope/vim-unimpaired"},
     {"tpope/vim-commentary"},
+    {"tpope/vim-repeat"},
     {"jremmen/vim-ripgrep"},
     {"christoomey/vim-tmux-navigator"},
+    {"alx741/vim-rustfmt"}
 }
