6d328e0c524fe94d198a452aab159a039b906edb
Tony Doan <tdoan@tdoan.com>
2025-04-14 20:43:22 -0700

    nvimtree - add hotkey leader-f for NvimTreeFindFileToggle

--- nvim/.config/nvim/lua/plugins/nvimtree.lua
+++ nvim/.config/nvim/lua/plugins/nvimtree.lua
@@ -1,8 +1,9 @@
 return {
     "nvim-tree/nvim-tree.lua",
     config = function()
-        require("nvim-tree").setup()
+        require("nvim-tree").setup({})
         local opts = {}
         vim.keymap.set('n', '<leader>n', [[:NvimTreeToggle<CR>]], opts)
+        vim.keymap.set('n', '<leader>f', [[:NvimTreeFindFileToggle<CR>]], opts)
     end
 }
